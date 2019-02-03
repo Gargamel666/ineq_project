e30ddb8d4dd43d2207f0eb55cbacc44d723ef91b
srh-gst <gust.sarah@outlook.de>
2019-02-03 15:26:02 +0100

    uses now inflation adjusted data, bigger legend, limits adjusted

--- reports/GER/code/create_plots.R
+++ reports/GER/code/create_plots.R
@@ -62,78 +62,78 @@ dev.off()
 # densities ------------------------------------------------------------------
 
 library(psych)
-silc.pos.p1.07 <- silc.pos.p1 %>% filter(rb010 == 2007)   
-silc.pos.p2.07 <- silc.pos.p2 %>% filter(rb010 == 2007)
-silc.pos.p1.12 <- silc.pos.p1 %>% filter(rb010 == 2012)   
-silc.pos.p2.12 <- silc.pos.p2 %>% filter(rb010 == 2012)
-silc.pos.p1.17 <- silc.pos.p1 %>% filter(rb010 == 2017)   
-silc.pos.p2.17 <- silc.pos.p2 %>% filter(rb010 == 2017)
-describeBy(silc.pos.p1$income_p1_1, silc.pos.p1$rb010)
-describeBy(silc.pos.p2$income_p2_1, silc.pos.p2$rb010)
-describeBy(silc.pos.p2$income_p2_2, silc.pos.p2$rb010)
-hist(silc.pos.p1.06$income_p1_1)
+silc.p1.07 <- silc.p1 %>% filter(rb010 == 2007)   
+silc.p2.07 <- silc.p2 %>% filter(rb010 == 2007)
+silc.p1.12 <- silc.p1 %>% filter(rb010 == 2012)   
+silc.p2.12 <- silc.p2 %>% filter(rb010 == 2012)
+silc.p1.17 <- silc.p1 %>% filter(rb010 == 2017)   
+silc.p2.17 <- silc.p2 %>% filter(rb010 == 2017)
+describeBy(silc.p1$income_p1_1, silc.p1$rb010)
+describeBy(silc.p2$income_p2_1, silc.p2$rb010)
+describeBy(silc.p2$income_p2_2, silc.p2$rb010)
+hist(silc.p1.06$income_p1_1)
 
 
 
 png(file = "densityp11.png")
-plot(density(silc.pos.p1.07$income_p1_1), main = "",
-     xlim = c(0, 200000), ylim = c(0, 7e-05))
-lines(density(silc.pos.p1.12$income_p1_1), lty = 2)
-lines(density(silc.pos.p1.17$income_p1_1), lty = 3)
+plot(density(silc.p1.07$income_p1_1), main = "",
+     xlim = c(0, 100000), ylim = c(0, 7e-05))
+lines(density(silc.p1.12$income_p1_1), lty = 2)
+lines(density(silc.p1.17$income_p1_1), lty = 3)
 legend("topright", legend=c("2007", 
                             "2012", "2017"),
-       lty=1:3, cex = 0.8)
+       lty=1:3, cex = 2)
 dev.off()
 
 png(file = "densityp11closer.png")
-plot(density(silc.pos.p1.07$income_p1_1), main = "",
+plot(density(silc.p1.07$income_p1_1), main = "",
      xlim = c(0, 50000), ylim = c(0, 7e-05))
-lines(density(silc.pos.p1.12$income_p1_1), lty = 2)
-lines(density(silc.pos.p1.17$income_p1_1), lty = 3)
+lines(density(silc.p1.12$income_p1_1), lty = 2)
+lines(density(silc.p1.17$income_p1_1), lty = 3)
 legend("topright", legend=c("2007", 
                             "2012", "2017"),
-       lty=1:3, cex = 0.8)
+       lty=1:3, cex = 2)
 dev.off()
 
 png(file = "densityp21.png")
-plot(density(silc.pos.p2.07$income_p2_1), "",
-     xlim = c(0, 200000), ylim = c(0, 7e-05))
-lines(density(silc.pos.p2.12$income_p2_1), lty = 2)
-lines(density(silc.pos.p2.17$income_p2_1), lty = 3)
+plot(density(silc.p2.07$income_p2_1), "",
+     xlim = c(0, 100000), ylim = c(0, 7e-05))
+lines(density(silc.p2.12$income_p2_1), lty = 2)
+lines(density(silc.p2.17$income_p2_1), lty = 3)
 legend("topright", legend=c("2007", 
                             "2012", "2017"),
-       lty=1:3, cex = 0.8)
+       lty=1:3, cex = 2)
 dev.off()
 
 
 png(file = "densityp13.png")
-plot(density(silc.pos.p1.07$income_p1_3), main = "",
-     xlim = c(0, 200000), ylim = c(0, 7e-05))
-lines(density(silc.pos.p1.12$income_p1_3), lty = 2)
-lines(density(silc.pos.p1.17$income_p1_3), lty = 3)
+plot(density(silc.p1.07$income_p1_3), main = "",
+     xlim = c(0, 100000), ylim = c(0, 7e-05))
+lines(density(silc.p1.12$income_p1_3), lty = 2)
+lines(density(silc.p1.17$income_p1_3), lty = 3)
 legend("topright", legend=c("2007", 
                             "2012", "2017"),
-       lty=1:3, cex = 0.8)
+       lty=1:3, cex = 2)
 dev.off()
 
 png(file = "densityp13closer.png")
-plot(density(silc.pos.p1.07$income_p1_3), main = "",
+plot(density(silc.p1.07$income_p1_3), main = "",
      xlim = c(0, 50000), ylim = c(0, 7e-05))
-lines(density(silc.pos.p1.12$income_p1_3), lty = 2)
-lines(density(silc.pos.p1.17$income_p1_3), lty = 3)
+lines(density(silc.p1.12$income_p1_3), lty = 2)
+lines(density(silc.p1.17$income_p1_3), lty = 3)
 legend("topright", legend=c("2007", 
                             "2012", "2017"),
-       lty=1:3, cex = 0.8)
+       lty=1:3, cex = 2)
 dev.off()
 
 png(file = "densityp23.png")
-plot(density(silc.pos.p2.07$income_p2_3), "",
-     xlim = c(0, 200000), ylim = c(0, 7e-05))
-lines(density(silc.pos.p2.12$income_p2_3), lty = 2)
-lines(density(silc.pos.p2.17$income_p2_3), lty = 3)
+plot(density(silc.p2.07$income_p2_3), "",
+     xlim = c(0, 100000), ylim = c(0, 7e-05))
+lines(density(silc.p2.12$income_p2_3), lty = 2)
+lines(density(silc.p2.17$income_p2_3), lty = 3)
 legend("topright", legend=c("2007", 
                             "2012", "2017"),
-       lty=1:3, cex = 0.8)
+       lty=1:3, cex = 2)
 dev.off()
 
 # Lorenz curves ----------------------------------------------------------------
@@ -141,31 +141,21 @@ dev.off()
 library(ineq)
 
 png(file = "Lorenzp1.png")
-plot(Lc(silc.pos.p1.17$income_p1_1), main = "")
-lines(Lc(silc.pos.p1.17$income_p1_2), lty = 2)
-lines(Lc(silc.pos.p1.17$income_p1_3), lty = 3)
+plot(Lc(silc.p1.17$income_p1_1), main = "")
+lines(Lc(silc.p1.17$income_p1_2), lty = 2)
+lines(Lc(silc.p1.17$income_p1_3), lty = 3)
 legend("topleft", legend=c("Pre-tax factor income", 
                             "Pre-tax national income", "Post-tax disposable income"),
-       lty=1:3, cex = 0.8)
+       lty=1:3, cex = 1.3)
 dev.off()
 
 
 png(file = "Lorenzp2.png")
-plot(Lc(silc.pos.p2.17$income_p2_1), main = "")
-lines(Lc(silc.pos.p2.17$income_p2_2), lty = 2)
-lines(Lc(silc.pos.p2.17$income_p2_3), lty = 3)
+plot(Lc(silc.p2.17$income_p2_1), main = "")
+lines(Lc(silc.p2.17$income_p2_2), lty = 2)
+lines(Lc(silc.p2.17$income_p2_3), lty = 3)
 legend("topleft", legend=c("Pre-tax factor income", 
                            "Pre-tax national income", "Post-tax disposable income"),
-       lty=1:3, cex = 0.8)
+       lty=1:3, cex = 1.3)
 dev.off()
 
-
-
-png(file = "Lorenzp2.png")
-plot(Lc(silc.pos.p2.17$income_p2_1), main = "")
-lines(Lc(silc.pos.p2.17$income_p2_2), lty = 2)
-lines(Lc(silc.pos.p2.17$income_p2_3), lty = 3)
-legend("topleft", legend=c("Pre-tax factor income", 
-                           "Pre-tax national income", "Post-tax disposable income"),
-       lty=1:3, cex = 0.8)
-dev.off()
